178ce60874c80ed2cde20c0e0486d02d93ea92bc
Eugene Komissarov <ekom@cian.ru>
2021-11-08 18:58:48 +0300

    vertical lines

--- colab/sunset.py
+++ colab/sunset.py
@@ -205,58 +205,136 @@ def triad(item):
             return {k[0]: f"{a_flag}{k[1]}" for k in i.items()}
 
 
-def plot_basic_rolling(*lines, items=["events", "cpa"], line_colors=["darkblue", "orange"]):
-    fig, axN = plt.subplots(len(lines), 1)
+class BasicDynamics:
+    def __init__(self, df, what, plot_ev_per_click, vert_lines):
+        self.data = df
+        self.what = what
+        self.plot_ev_per_click = plot_ev_per_click
+        self.vert_lines = vert_lines
+        if what is None:
+            self.what = {"events", "events_ss", "events_fdv", "ads", "ipotek", "ct", "common",
+                    "events_commercial", "events_salesub", "events_rentsub", "events_saleflats", "events_rentflats",
+                    "events_applications",
+                    "conv_agg_full", "conv_agg_owners",
+                    "impressions"}
 
-    for i, line in enumerate(lines):
-        axN[i].plot(line.index, line['values'], '-', color=line_colors[i])
-        axN[i].plot(line.index, line['rolling_mean'], '--', color='gray')
-        axN[i].plot(line.index, line['rolling_std'], ':', color='gray')
-        axN[i].lines[1].set_alpha(0.5)
-        axN[i].lines[2].set_alpha(0.5)
-        locator = mdates.AutoDateLocator(minticks=0, maxticks=3)
+    @staticmethod
+    def scale_plot_size(w=12, h=14):
+        scale_plot_size(w, h)
+
+    def run_plot(self):
+        if "events" in self.what:
+            self._plt_basic_dyn(ev="events", cpa="cpa", ev_per_click="ev_per_click",
+                                item_labels=["phone events", "cpa", "conv%"])
+        if "events_ss" in self.what:
+            self._plt_basic_dyn(ev="events_ss", cpa="cpa_ss", ev_per_click="ev_ss_per_click",
+                                item_labels=["ss uniq events", "cpa_ss", "conv%"])
+        if "events_fdv" in self.what:
+            self._plt_basic_dyn(ev="events_fdv", cpa="cpa_fdv", ev_per_click="ev_fdv_per_click",
+                                item_labels=["fdv phone events", "cpa_fdv", "conv%"])
+        if "events_commercial" in self.what:
+            self._plt_basic_dyn(ev="events_commercial", cpa="cpa_commercial", ev_per_click="ev_commercial_per_click",
+                                item_labels=["commercial phone events", "cpa_commercial", "conv%"])
+        if "events_salesub" in self.what:
+            self._plt_basic_dyn(ev="events_salesub", cpa="cpa_salesub", ev_per_click="ev_salesub_per_click",
+                                item_labels=["salesub phone events", "cpa_salesub", "conv%"])
+        if "events_rentsub" in self.what:
+            self._plt_basic_dyn(ev="events_rentsub", cpa="cpa_rentsub", ev_per_click="ev_rentsub_per_click",
+                                item_labels=["rentsub phone events", "cpa_rentsub", "conv%"])
+        if "events_saleflats" in self.what:
+            self._plt_basic_dyn(ev="events_saleflats", cpa="cpa_saleflats", ev_per_click="ev_saleflats_per_click",
+                                item_labels=["saleflats phone events", "cpa_saleflats", "conv%"])
+        if "events_rentflats" in self.what:
+            self._plt_basic_dyn(ev="events_rentflats", cpa="cpa_rentflats", ev_per_click="ev_rentflats_per_click",
+                                item_labels=["rentflats phone events", "cpa_rentflats", "conv%"])
+        if "events_applications" in self.what:
+            self._plt_basic_dyn(ev="events_applications", cpa="cpa_applications", ev_per_click="ev_applications_per_click",
+                                item_labels=["applications realtors events", "cpa_applications", "conv%"])
+        if "ads" in self.what:
+            self._plt_basic_dyn(ev="ads", cpa="cpad", ev_per_click="ad_per_click",
+                           item_labels=["ads", "cpad", "conv%"])
+        if "conv_agg_owners" in self.what:
+            self._plt_basic_dyn(ev="conv_agg_owners", cpa="cp_agg_owners", ev_per_click="agg_owners_per_click",
+                           item_labels=["ads+appl", "cpad", "conv%"])
+        if "ipotek" in self.what:
+            self._plt_basic_dyn(ev="ipotek", cpa="cpa_ipotek", ev_per_click="ipotek_per_click",
+                           item_labels=["ipotek", "cpa_ipotek", "conv%"])
+        if "ct" in self.what:
+            self._plt_basic_dyn(ev="ct", cpa="cpa_ct", ev_per_click="ct_per_click",
+                           item_labels=["ct", "cpa_ct", "conv%"])
+        if "conv_agg_full" in self.what:
+            self._plt_basic_dyn(ev="conv_agg_full", cpa="cp_agg_full", ev_per_click="agg_full_per_click",
+                           item_labels=["aggregate", "cpa", "conv%"])
+
+        if "impressions" in self.what:
+            self._plt_basic_dyn(ev="impressions", cpa="cpm", ev_per_click="ctr",
+                           item_labels=["impressions", "cpm", "ctr"])
+
+        if "common" in self.what:
+            cost_rur = self.data.loc[:, "cost_rur"]
+            cpc = self.data.loc[:, "cpc"]
+            ctr = self.data.loc[:, "ctr"]
+            clicks = self.data.loc[:, "clicks"]
+            rolling_cost_rur = cost_rur.rolling(7, center=True)
+            rolling_cpc = cpc.rolling(7, center=True)
+            rolling_ctr = ctr.rolling(7, center=True)
+            rolling_clicks = clicks.rolling(7, center=True)
+            cost_rur = pd.DataFrame({'values': cost_rur, 'rolling_mean': rolling_cost_rur.mean(), 'rolling_std': rolling_cost_rur.std()})
+            cpc = pd.DataFrame({'values': cpc, 'rolling_mean': rolling_cpc.mean(), 'rolling_std': rolling_cpc.std()})
+            ctr = pd.DataFrame({'values': ctr, 'rolling_mean': rolling_ctr.mean(), 'rolling_std': rolling_ctr.std()})
+            clicks = pd.DataFrame({'values': clicks, 'rolling_mean': rolling_clicks.mean(), 'rolling_std': rolling_clicks.std()})
+            self.plot_basic_rolling(cost_rur, cpc, ctr, clicks,
+                                    items=["cost_rur", "cpc", "ctr", "clicks"],
+                                    line_colors=["darkblue", "orange", "green", "red"])
+
+    def _plt_basic_dyn(self, ev, cpa, ev_per_click, item_labels):
+        events = self.data.loc[:, ev]
+        cpas = self.data.loc[:, cpa]
+        rolling_ev = events.rolling(7, center=True)
+        rolling_cpas = cpas.rolling(7, center=True)
+        events = pd.DataFrame({'values': events, 'rolling_mean': rolling_ev.mean(), 'rolling_std': rolling_ev.std()})
+        cpas = pd.DataFrame({'values': cpas, 'rolling_mean': rolling_cpas.mean(), 'rolling_std': rolling_cpas.std()})
+        if np.isinf(cpas.iloc[0]['values']):
+            cpas.iloc[0]['values'] = 0
+        if self.plot_ev_per_click:
+            ev_per_click = self.data.loc[:, ev_per_click]
+            rolling_convsperclick = ev_per_click.rolling(7, center=True)
+            ev_per_click = pd.DataFrame({'values': ev_per_click, 'rolling_mean': rolling_convsperclick.mean(),
+                                         'rolling_std': rolling_convsperclick.std()})
+            self.plot_basic_rolling(events, cpas, ev_per_click, items=item_labels,
+                                    line_colors=["darkblue", "orange", "darkgreen"])
+        else:
+            self.plot_basic_rolling(events, cpas, items=item_labels[:-1],
+                                    line_colors=["darkblue", "orange"])
+
+    def plot_basic_rolling(self, *lines, items=["events", "cpa"], line_colors=["darkblue", "orange"]):
+        fig, axN = plt.subplots(len(lines), 1)
+
+        for i, line in enumerate(lines):
+            axN[i].plot(line.index, line['values'], '-', color=line_colors[i])
+            axN[i].plot(line.index, line['rolling_mean'], '--', color='gray')
+            axN[i].plot(line.index, line['rolling_std'], ':', color='gray')
+            axN[i].lines[1].set_alpha(0.5)
+            axN[i].lines[2].set_alpha(0.5)
+            locator = mdates.AutoDateLocator(minticks=0, maxticks=3)
+            formatter = mdates.ConciseDateFormatter(locator)
+            axN[i].legend([items[i], 'rolling_mean', 'rolling_std'])
+            axN[i].xaxis.set_major_locator(locator)
+            axN[i].xaxis.set_major_formatter(formatter)
+            for j in self.vert_lines:
+                axN[i].axvline(x=j, color='gray')
+
+        locator = mdates.AutoDateLocator(minticks=5, maxticks=20)
         formatter = mdates.ConciseDateFormatter(locator)
-        axN[i].legend([items[i], 'rolling_mean', 'rolling_std'])
         axN[i].xaxis.set_major_locator(locator)
         axN[i].xaxis.set_major_formatter(formatter)
-
-    locator = mdates.AutoDateLocator(minticks=5, maxticks=20)
-    formatter = mdates.ConciseDateFormatter(locator)
-    axN[i].xaxis.set_major_locator(locator)
-    axN[i].xaxis.set_major_formatter(formatter)
-    plt.show()
+        plt.show()
 
 
-def _plt_basic_dyn(tt, ev, cpa, ev_per_click, item_labels, plot_ev_per_click = False):
-    events = tt.loc[:, ev]
-    cpas = tt.loc[:, cpa]
-    rolling_ev = events.rolling(7, center=True)
-    rolling_cpas = cpas.rolling(7, center=True)
-    events = pd.DataFrame({'values': events, 'rolling_mean': rolling_ev.mean(), 'rolling_std': rolling_ev.std()})
-    cpas = pd.DataFrame({'values': cpas, 'rolling_mean': rolling_cpas.mean(), 'rolling_std': rolling_cpas.std()})
-    if np.isinf(cpas.iloc[0]['values']):
-        cpas.iloc[0]['values'] = 0
-    if plot_ev_per_click:
-        ev_per_click = tt.loc[:, ev_per_click]
-        rolling_convsperclick = ev_per_click.rolling(7, center=True)
-        ev_per_click = pd.DataFrame({'values': ev_per_click, 'rolling_mean': rolling_convsperclick.mean(),
-                                     'rolling_std': rolling_convsperclick.std()})
-        plot_basic_rolling(events, cpas, ev_per_click, items=item_labels,
-                           line_colors=["darkblue", "orange", "darkgreen"])
-    else:
-        plot_basic_rolling(events, cpas, items=item_labels[:-1])
-
-
-def plot_basic_dynamics(df, what=None, region_filters=None, campaign_filters=None, system_filters=None, plot_ev_per_click = False):
-    grp = ['date']
+def plot_basic_dynamics(df, what=None,
+                        region_filters=None, campaign_filters=None, system_filters=None,
+                        plot_ev_per_click = False, vert_lines=None):
     tt = df.copy()
-    if what is None:
-        what = {"events", "events_ss", "events_fdv", "ads", "ipotek", "ct", "common",
-                "events_commercial", "events_salesub", "events_rentsub", "events_saleflats", "events_rentflats",
-                "events_applications",
-                "conv_agg_full", "conv_agg_owners",
-                "impressions"}
-
     if region_filters:
         tt = tt[tt.region.isin(region_filters)]
 
@@ -269,78 +347,13 @@ def plot_basic_dynamics(df, what=None, region_filters=None, campaign_filters=Non
             campaign_mask = campaign_mask | (tt.campaignname.str.contains(i))
         tt = tt[campaign_mask]
 
-    tt = tt.groupby(grp).sum()
+    tt = tt.groupby(['date']).sum()
     tt = calc_base_values(tt)
     tt.index = pd.to_datetime(tt.index)
+    basicdyn = BasicDynamics(tt, what, plot_ev_per_click, vert_lines)
+    basicdyn.run_plot()
 
-    scale_plot_size(12, 14)
-    if "events" in what:
-        _plt_basic_dyn(tt, ev="events", cpa="cpa", ev_per_click="ev_per_click",
-                       item_labels=["phone events", "cpa", "conv%"], plot_ev_per_click=plot_ev_per_click)
-    if "events_ss" in what:
-        _plt_basic_dyn(tt, ev="events_ss", cpa="cpa_ss", ev_per_click="ev_ss_per_click",
-                       item_labels=["ss uniq events", "cpa_ss", "conv%"], plot_ev_per_click=plot_ev_per_click)
-    if "events_fdv" in what:
-        _plt_basic_dyn(tt, ev="events_fdv", cpa="cpa_fdv", ev_per_click="ev_fdv_per_click",
-                       item_labels=["fdv phone events", "cpa_fdv", "conv%"], plot_ev_per_click=plot_ev_per_click)
-    if "events_commercial" in what:
-        _plt_basic_dyn(tt, ev="events_commercial", cpa="cpa_commercial", ev_per_click="ev_commercial_per_click",
-                       item_labels=["commercial phone events", "cpa_commercial", "conv%"], plot_ev_per_click=plot_ev_per_click)
-    if "events_salesub" in what:
-        _plt_basic_dyn(tt, ev="events_salesub", cpa="cpa_salesub", ev_per_click="ev_salesub_per_click",
-                       item_labels=["salesub phone events", "cpa_salesub", "conv%"], plot_ev_per_click=plot_ev_per_click)
-    if "events_rentsub" in what:
-        _plt_basic_dyn(tt, ev="events_rentsub", cpa="cpa_rentsub", ev_per_click="ev_rentsub_per_click",
-                       item_labels=["rentsub phone events", "cpa_rentsub", "conv%"], plot_ev_per_click=plot_ev_per_click)
-    if "events_saleflats" in what:
-        _plt_basic_dyn(tt, ev="events_saleflats", cpa="cpa_saleflats", ev_per_click="ev_saleflats_per_click",
-                       item_labels=["saleflats phone events", "cpa_saleflats", "conv%"], plot_ev_per_click=plot_ev_per_click)
-    if "events_rentflats" in what:
-        _plt_basic_dyn(tt, ev="events_rentflats", cpa="cpa_rentflats", ev_per_click="ev_rentflats_per_click",
-                       item_labels=["rentflats phone events", "cpa_rentflats", "conv%"], plot_ev_per_click=plot_ev_per_click)
-    if "events_applications" in what:
-        _plt_basic_dyn(tt, ev="events_applications", cpa="cpa_applications", ev_per_click="ev_applications_per_click",
-                       item_labels=["applications realtors events", "cpa_applications", "conv%"], plot_ev_per_click=plot_ev_per_click)
-    if "ads" in what:
-        _plt_basic_dyn(tt, ev="ads", cpa="cpad", ev_per_click="ad_per_click",
-                       item_labels=["ads", "cpad", "conv%"], plot_ev_per_click=plot_ev_per_click)
-    if "conv_agg_owners" in what:
-        _plt_basic_dyn(tt, ev="conv_agg_owners", cpa="cp_agg_owners", ev_per_click="agg_owners_per_click",
-                       item_labels=["ads+appl", "cpad", "conv%"], plot_ev_per_click=plot_ev_per_click)
-    if "ipotek" in what:
-        _plt_basic_dyn(tt, ev="ipotek", cpa="cpa_ipotek", ev_per_click="ipotek_per_click",
-                       item_labels=["ipotek", "cpa_ipotek", "conv%"], plot_ev_per_click=plot_ev_per_click)
-    if "ct" in what:
-        _plt_basic_dyn(tt, ev="ct", cpa="cpa_ct", ev_per_click="ct_per_click",
-                       item_labels=["ct", "cpa_ct", "conv%"], plot_ev_per_click=plot_ev_per_click)
-    if "conv_agg_full" in what:
-        _plt_basic_dyn(tt, ev="conv_agg_full", cpa="cp_agg_full", ev_per_click="agg_full_per_click",
-                       item_labels=["aggregate", "cpa", "conv%"], plot_ev_per_click=plot_ev_per_click)
-
-    if "impressions" in what:
-        _plt_basic_dyn(tt, ev="impressions", cpa="cpm", ev_per_click="ctr",
-                       item_labels=["impressions", "cpm", "ctr"],
-                       plot_ev_per_click=plot_ev_per_click)
-
-    if "common" in what:
-        cost_rur = tt.loc[:, "cost_rur"]
-        cpc = tt.loc[:, "cpc"]
-        ctr = tt.loc[:, "ctr"]
-        clicks = tt.loc[:, "clicks"]
-        rolling_cost_rur = cost_rur.rolling(7, center=True)
-        rolling_cpc = cpc.rolling(7, center=True)
-        rolling_ctr = ctr.rolling(7, center=True)
-        rolling_clicks = clicks.rolling(7, center=True)
-        cost_rur = pd.DataFrame({'values': cost_rur, 'rolling_mean': rolling_cost_rur.mean(), 'rolling_std': rolling_cost_rur.std()})
-        cpc = pd.DataFrame({'values': cpc, 'rolling_mean': rolling_cpc.mean(), 'rolling_std': rolling_cpc.std()})
-        ctr = pd.DataFrame({'values': ctr, 'rolling_mean': rolling_ctr.mean(), 'rolling_std': rolling_ctr.std()})
-        clicks = pd.DataFrame({'values': clicks, 'rolling_mean': rolling_clicks.mean(), 'rolling_std': rolling_clicks.std()})
-        plot_basic_rolling(cost_rur, cpc, ctr, clicks,
-                           items=["cost_rur", "cpc", "ctr", "clicks"],
-                           line_colors=["darkblue", "orange", "green", "red"])
-
-
-def plot_avg_position_yandex(df, region_filters=None, campaign_filters=None):
+def plot_avg_position_yandex(df, region_filters=None, campaign_filters=None, vert_lines=None):
     grp = ['date']
     df = df[(df.system == "y") & (df.campaignname.str.contains("_search"))]
 
@@ -363,11 +376,15 @@ def plot_avg_position_yandex(df, region_filters=None, campaign_filters=None):
 
     scale_plot_size(12, 12)
     plots = ["impr_pos", "click_pos", "traffic_vol"]
-    tt.loc[:, plots].plot(subplots=True)
+    ax = tt.loc[:, plots].plot(subplots=True)
+    if vert_lines:
+        for i in ax:
+            for j in vert_lines:
+                i.axvline(x=j, color='gray')
     plt.show()
 
 
-def plot_top_is_position_google(df, region_filters=None, campaign_filters=None):
+def plot_top_is_position_google(df, region_filters=None, campaign_filters=None, vert_lines=None):
     grp = ['date']
     df = df[(df.system == "g") & (df.campaignname.str.contains("_search"))]
 
@@ -388,12 +405,17 @@ def plot_top_is_position_google(df, region_filters=None, campaign_filters=None):
 
     scale_plot_size(12, 12)
     plots = ["top_is", "abstop_is"]
-    tt.loc[:, plots].plot(subplots=True)
+    ax = tt.loc[:, plots].plot(subplots=True)
+    if vert_lines:
+        for i in ax:
+            for j in vert_lines:
+                i.axvline(x=j, color='gray')
     plt.show()
 
 
 def plot_compare_base(data, y_value, group_by_plot, plot_set,
-                 region_filters=None, campaign_filters=None, system_filters=None, ymax=None, vert_lines=None):
+                      region_filters=None, campaign_filters=None, system_filters=None,
+                      ymax=None, vert_lines=None):
 
     if type(y_value) is not list:
         y_value = [y_value]
