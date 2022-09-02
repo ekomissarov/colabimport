2269eef31309541daf5e7feea204904fc6dc029c
Eugene Komissarov <ekom@cian.ru>
2022-09-02 16:53:10 +0300

    fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysea-colabimport", # Replace with your own username
-    version="0.0.157",
+    version="0.0.158",
     author="Eugene Komissarov",
     author_email="ekom@cian.ru",
     description="Colab functions base",
