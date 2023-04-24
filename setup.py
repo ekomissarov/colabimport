679596bd61422426b48e3630b2c48258f2c5cf56
Eugene Komissarov <ekom@cian.ru>
2023-04-24 20:05:34 +0300

    sum numeric_only=True

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysea-colabimport", # Replace with your own username
-    version="0.0.235",
+    version="0.0.236",
     author="Eugene Komissarov",
     author_email="ekom@cian.ru",
     description="Colab functions base",
