5af435e0e3aca8d32810c9737484e50e9708400b
Eugene Komissarov <ekom@cian.ru>
2023-05-26 19:40:57 +0300

    fix

--- colab/sunset.py
+++ colab/sunset.py
@@ -692,7 +692,7 @@ def plot_compare_base(data, y_value, group_by_plot, plot_set,
     for j in y_value:
         plotdata = pd.DataFrame({i: tt.loc[i][j] for i in plot_set})
         for i in plotdata:
-            plotdata[i].plot(label="{} {}: {}".format(j, group_by_plot, i))
+            plotdata[i].plot(label="{}: {}".format(group_by_plot, i))
 
     if ymax is not None:
         #plt.xlim(right=xmax)  # xmax is your value
